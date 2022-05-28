25e51f5e8129f0af593d0618df7735202f381e7b
Tzahi Levi <tazachil@gmail.com>
2022-05-28 11:04:38 +0300

    Ram function completed

--- hw2_spring2022/Tests/mytest.py
+++ hw2_spring2022/Tests/mytest.py
@@ -14,12 +14,29 @@ from Business.Disk import Disk
 
 class Test(AbstractTest):
     def test_Create(self) -> None:
+        #Disk test
         self.assertEqual(Status.OK, Solution.addDisk(Disk(1, "DELL", 10, 10, 10)), "Should work")
+        self.assertEqual(Status.OK, Solution.addDisk(Disk(2, "ASUS", 2, 5, 20)), "Should work")
+        disk = Solution.getDiskByID(1)
+        disk.__str__()
         self.assertEqual(Status.OK, Solution.deleteDisk(1), "Should work")
-        self.assertEqual(Status.OK, Solution.deleteDisk(2), "Should through exception")
+        self.assertEqual(Status.NOT_EXISTS, Solution.deleteDisk(3), "Should through exception")
+        self.assertEqual(Status.ALREADY_EXISTS, Solution.addDisk(Disk(2, "ASUS", 10, 10, 10)), "Should through exception")
+
+        #RAM test
+        self.assertEqual(Status.OK, Solution.addRAM(RAM(1, "DELL", 10)), "Should work")
+        self.assertEqual(Status.OK, Solution.addRAM(RAM(2, "ASUS", 2)), "Should work")
+        ram = Solution.getRAMByID(1)
+        ram.__str__()
+        self.assertEqual(Status.OK, Solution.deleteRAM(1), "Should work")
+        self.assertEqual(Status.NOT_EXISTS, Solution.deleteRAM(3), "Should through exception")
+        self.assertEqual(Status.ALREADY_EXISTS, Solution.addRAM(RAM(2, "ASUS", 10)), "Should through exception")
+
         pass
 
 
 # *** DO NOT RUN EACH TEST MANUALLY ***
 if __name__ == '__main__':
+    Solution.dropTables()
     unittest.main(verbosity=2, exit=False)
+
